668ac92d8ba884e14442e91d454d6a4e851aa546
Jason Hargrove <rijaha@gmail.com>
2022-12-02 14:33:58 -0600

    Bind first image and made ref.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref } from "vue";
+import socksGreenImage from './assets/images/socks_green.jpeg'
 
 const useChangeWithDelay = (state, newVal, delay) => {
   setTimeout(() => {
@@ -8,6 +9,7 @@ const useChangeWithDelay = (state, newVal, delay) => {
 };
 
 const product = ref("Socks");
+const image = ref(socksGreenImage)
 
 useChangeWithDelay(product, "New Socks", 1000);
 </script>
@@ -16,9 +18,13 @@ useChangeWithDelay(product, "New Socks", 1000);
   <div class="nav-bar"></div>
   <div class="product-display">
     <div class="product-container">
-      <div class="product-image"><!-- image goes here --></div>
+      <div class="product-image">
+        <img v-bind:src="image">
+      </div>
       <div class="product-info">
         <h1>{{ product }}</h1>
+        <p>In Stock</p>
+        <p>Out of Stock</p>
       </div>
     </div>
   </div>
