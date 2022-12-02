92c38688072012921a89aa181e47da9c69995283
Jason Hargrove <rijaha@gmail.com>
2022-12-02 14:25:09 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from "vue";
+
+const useChangeWithDelay = (state, newVal, delay) => {
+  setTimeout(() => {
+    state.value = newVal;
+  }, delay);
+};
+
+const product = ref("Socks");
+
+useChangeWithDelay(product, "New Socks", 1000);
+</script>
+
+<template>
+  <div class="nav-bar"></div>
+  <div class="product-display">
+    <div class="product-container">
+      <div class="product-image"><!-- image goes here --></div>
+      <div class="product-info">
+        <h1>{{ product }}</h1>
+      </div>
+    </div>
+  </div>
+</template>
